a72af9f19f81d72849e1316b788ab002da13a8e5
Damian Lattenero <damianlattenero@gmail.com>
2015-03-18 17:36:29 -0300

    firs commit

---
+++ build.sbt
@@ -0,0 +1,10 @@
+name := "obj3-tp1-parte1-mixine"
+
+version := "1.0"
+
+scalaVersion := "2.11.6"
+
+libraryDependencies += "org.scalatest" % "scalatest_2.11" % "2.2.4" % "test"
+
+
+
---
+++ src/test/scala/WorldSample/World.scala
@@ -0,0 +1,14 @@
+package WorldSample
+
+import org.scalatest.FlatSpec
+
+/**
+ * Created by damian on 18/03/15.
+ */
+class World extends FlatSpec{
+
+  "esto por favor" should "funcionar de una vez" in {
+    ???
+  }
+
+}
